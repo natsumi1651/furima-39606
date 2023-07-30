7ea80947cb7f82d692f9b20cdfe8be4dc4ce77ec
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-07-30 17:59:00 +0900

    active Hash途中まで

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  validates :name, presence: true, unless: :was_attached?
+  
+  belongs_to :user
 end
--- db/migrate/20230726024612_create_items.rb
+++ db/migrate/20230726024612_create_items.rb
@@ -1,14 +1,15 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-      t.name :string, null: false
-      t.description :text, null: false
-      t.category_id :integer, null: false
-      t.status_id :integer, null: false
-      t.shipmentfee_id :integer, null: false
-      t.prefecture_id :integer, null: false
-      t.waitingday_id :integer, null: false
-      t.item_price :integer, null: false
+    
+      t.string :name, null: false
+      t.text, :description, null: false
+      t.integer :category_id, null: false
+      t.integer :status_id, null: false
+      t.integer :shipmentfee_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :waitingday_id, null: false
+      t.integer :item_price, null: false
       t.timestamps, null: false
    
     end
