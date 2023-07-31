0b0da5d10b45e80371872da089cddbd71ad1bdc1
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-07-31 21:26:52 +0900

    エラーメッセージの設定

--- app/models/article.rb
+++
@@ -1,5 +0,0 @@
-class Article < ApplicationRecord
-
-
-  
-end
--- app/models/item.rb
+++ app/models/item.rb
@@ -17,14 +17,10 @@ class Item < ApplicationRecord
   validates :item_price, presence: true, numericality: {
     greater_than_or_equal_to: 300,
     less_than_or_equal_to: 9999999,
+    only_integer: true,
   }
-  validates :item_price, format: { with: /\A[0-9]+\z/}
-
+  validates :item_price, format: { with: /\A[0-9]+\z/ }
   validates :image, presence: true
 
   belongs_to :user
 end
-
-#validates :name, presence: true, unless: :was_attached?
-#unless: :was_attached?はいるか？
-#validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
