c8e9ea0a73f793ab14785d0c8c31920719f4cc7c
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-07-31 12:28:01 +0900

    new/createの一部設定、hashの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,16 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to '/'
+  end
+
   def item_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :shipmentfee_id, :prefecture_id, :waitingday_id, :item_price,  :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
   has_one_attached :image
-  validates :name, presence: true, unless: :was_attached?
-  
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipmentfee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :waitingday_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_price, presence: true
+  validates :image, presence: true
+
+
   belongs_to :user
 end
+
+#validates :name, presence: true, unless: :was_attached?
+#unless: :was_attached?はいるか？
+#validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
