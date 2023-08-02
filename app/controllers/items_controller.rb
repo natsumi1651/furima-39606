a26518f7fdb859819aad9dac4f54f8c691e51d4e
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-08-02 20:17:15 +0900

    削除ボタン実装完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :no_edit_authority, only: :edit
 
   def index
@@ -21,11 +21,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-   
   end
 
   def edit
-  
   end
 
   def update
@@ -34,6 +32,11 @@ class ItemsController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
@@ -45,6 +48,7 @@ class ItemsController < ApplicationController
 
   def move_to_index
     return if user_signed_in?
+
     redirect_to new_user_session_path
   end
 
