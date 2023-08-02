4c13f55b3ca30fd5c2ac544fdc061233d35eec2a
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-08-02 18:10:48 +0900

    Update 20230731073106_add_user_id_to_items.rb

--- db/migrate/20230731073106_add_user_id_to_items.rb
+++ db/migrate/20230731073106_add_user_id_to_items.rb
@@ -1,4 +1,4 @@
-class AddUserIdToItems < ActiveRecord::Migration[7.0]
+class AddUserToItems < ActiveRecord::Migration[7.0]
   def change
     add_column :items, :user, :references, null: false, foreign_key: true
   end
