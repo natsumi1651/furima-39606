fadd5296d9db18cc311306507b98b7e654a5c6b0
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-08-03 12:13:58 +0900

    order一部設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 devise_for :users
 root to: 'items#index'
 resources :items
-
+root to: 'orders#index', as: 'orders_root'
+resources :orders, only: [:create]
 end    
